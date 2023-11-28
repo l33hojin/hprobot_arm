d79f0c79d57c9c6204be24ffa36fa5a9ce4cf604
l33hojin <bumbba@nate.com>
2023-11-28 18:08:06 +0900

    test

--- src/hprobot.sh
+++ src/hprobot.sh
@@ -9,4 +9,4 @@ source ~/hprobot_arm_ws/devel/setup.bash
 #roslaunch interbotix_xsarm_moveit xsarm_moveit.launch use_fake:=true robot_model:=vx300 use_hprobot_node:=true dof:=5 use_moveit_rviz:=true
 
 roslaunch interbotix_xsarm_moveit xsarm_moveit.launch use_actual:=true robot_model:=vx300 use_hprobot_node:=true dof:=5 use_moveit_rviz:=false
-
+roslaunch mqtt_client standalone.launch
